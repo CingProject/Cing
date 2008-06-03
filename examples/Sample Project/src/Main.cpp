dacbb934939c634c4c87d00c6056344e686804d9
Julio Obelleiro <julio.obelleiro@gmail.com>
2008-06-03 00:50:41 +0000

    Image class. Support to load/save/copy/render images. Copy operator TextureQuad

---
+++ examples/Sample Project/src/Main.cpp
@@ -0,0 +1,25 @@
+#include "Vision.h"
+
+CREATE_APPLICATION( "Vision Demo" );
+
+
+void setup()
+{
+
+}
+
+void draw()
+{
+}
+
+void end()
+{
+}
+
+void mousePressed()
+{
+}
+
+void keyPressed()
+{
+}
--- vision/src/graphics/Image.cpp
+++ vision/src/graphics/Image.cpp
@@ -21,17 +21,53 @@
 
 #include "Image.h"
 
+// Common
+#include "common/Exception.h"
+#include "common/ResourceManager.h"
 
 namespace Graphics
 {
 
 /**
- * @internal
- * @brief Constructor. Initializes class attributes.
+ * @brief Constructor. This constructor does not load or creates any image.
  */
 Image::Image():
   m_bIsValid( false )
 {
+}
+
+/**
+ * @brief Creates an image with the same data as the received image
+ *
+ * @param
+ */
+ Image::Image( const Image& img )
+{
+	init( img );
+}
+
+/**
+ * @brief Creates an image of the specified size, but does not load it from file.
+ * Therefore, the image created is empty. However, this image can be modified afterwards.
+ *
+ * @param width  Width of the image to be created
+ * @param height Height of the image to be created
+ * @param format Format of the image to be created. Possible formats are: RGB, ARGB, GRAY
+ */
+Image::Image( int width, int height, Format format /*= RGB*/ )
+{
+	init( width, height );
+}
+
+/**
+ * @brief Creates an image from a file. The image loaded can be modified afterwards.
+ * @note The image file specified should be placed on the data directory of the application
+ *
+ * @param name  Name of the file to be loaded. It must be placed in the data directory.
+ */
+Image::Image( const std::string& name )
+{
+	load( name );
 }
 
 /**
@@ -45,22 +81,78 @@ Image::~Image()
 }
 
 /**
- * @internal
- * @brief Initializes the class so it becomes valid.
+ * @brief Creates an image of the specified size, but does not load it from file.
+ * Therefore, the image created is empty. However, this image can be modified afterwards.
  *
- * @return true if the initialization was ok | false otherwise
+ * @param width  Width of the image to be created
+ * @param height Height of the image to be created
+ * @param format Format of the image to be created. Possible formats are: RGB, ARGB, GRAY
  */
-bool Image::init()
+void Image::init( int width, int height, Format format /*= RGB*/  )
 {
   // Check if the class is already initialized
   if ( isValid() )
-    return true;
+   THROW_EXCEPTION( "Image already initialized" );
 
+	// Create the empty image
+	THROW_EXCEPTION( "TODO" );
 
 	// The class is now initialized
 	m_bIsValid = true;
 
-	return true;
+}
+
+/**
+ * @brief Creates a copy of an image
+ *
+ * @param img Image to be copied
+ */
+void Image::init( const Image& img )
+{
+	this->operator =( img );
+	m_bIsValid = true;
+}
+
+/**
+ * @brief Creates an image from a file. The image loaded can be modified afterwards.
+ * 
+ * @note The image file specified should be placed on the data directory of the application
+ *
+ * @note Supported image formats are: .bmp, .jpg, .gif, .raw, .png, .tga and .dds.
+ *
+ * @param name  Name of the file to be loaded. It must be placed in the data directory.
+ */
+void Image::load( const std::string& name  )
+{
+	// Load file from disk
+	m_image.load( name, Common::ResourceManager::userResourcesGroupName );
+
+	// Create the texture quad (to draw image)
+	m_quad.init( m_image.getWidth(), m_image.getWidth(), m_image.getDepth() );
+  
+	// Load data to texture
+	m_quad.updateTexture( m_image );
+
+	// The class is now initialized
+	m_bIsValid = true;
+}
+
+/**
+ * @brief Saves the image to a file
+ *
+ * @note The type of image file that will be created depends on the extension passed
+ * For example, if you call myImage.save( "myFile.jpg" ), the image will be saved to a jpeg file
+ * with the name myFile.jpg
+ *
+ * @note The file will be placed in the application data folder
+ *
+ * @note Supported image formats are: .bmp, .jpg, .gif, .raw, .png, .tga and .dds.
+ * @param name Name that will have the image file
+ */
+void Image::save( const std::string& name )
+{
+	// Add the user app data folder to the name
+	m_image.save( Common::ResourceManager::userDataPath + name );
 }
 
 /**
@@ -74,17 +166,43 @@ void Image::end()
   if ( !isValid() )
     return;
 
+	m_quad.end();
+
 	// The class is not valid anymore
 	m_bIsValid = false;
 }
 
 /**
- * @internal
- * @brief Updates the class state
+ * @brief Draws the image in a specific position
+ *
+ * @param xPos x coordinate where the image should be drawn
+ * @param yPos y coordinate where the image should be drawn
+ * @param zPos z coordinate where the image should be drawn. If not passed, z will be set to zero
  */
-void Image::update()
+void Image::draw( int xPos, int yPos, int zPos /*= 0*/ )
 {
+	m_quad.setPosition( xPos, yPos, zPos );
+	m_quad.setVisible( true );
+}
 
+
+/**
+ * @brief Copies the received image into the current image
+ *
+ * @param other Image to copy
+ */
+void Image::operator=( const Image& other )
+{
+	// Check the other image is valid
+	if ( !other.isValid() )	
+		THROW_EXCEPTION( "Trying to copy an invalid image" );
+
+	// Copy the data
+	m_image = other.m_image;
+	m_quad  = other.m_quad;
+
+	// Now this image is valid
+	m_bIsValid = true;
 }
 
 } // namespace Graphics
