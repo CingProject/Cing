b0757654fd7147fe6014c4a6b0f9eca6e7a7931e
Jorge Cano <jcanofuentes@gmail.com>
2009-07-22 12:05:31 +0000

    Minor changes in examples

---
+++ example_projects/Simple_BasicApplication/src/Simple_BasicApplication.cpp
@@ -0,0 +1,41 @@
+#include "Cing.h"
+CREATE_APPLICATION( "Cing Demo" );
+
+// This example shows the basic about a Cing application
+// It draws a line from the center of the screen to the mouse.
+// Press the mouse to clear the background.
+
+void setup()
+{
+	// Init values for size and stroke color
+	size(800, 800);
+	stroke(255);
+}
+
+void draw()
+{
+	// Draw a line from the center of the window to the mouse
+	line(width/2, height/2, mouseX, mouseY);
+}
+
+void end()
+{
+}
+
+void mousePressed()
+{
+	// Clear the background when the mouse is pressed
+	background(192, 64, 0);
+}
+
+void mouseMoved()
+{
+}
+
+void mouseReleased()
+{
+}
+
+void keyPressed()
+{
+}
