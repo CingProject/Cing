ca5c3389ab7c55fd34eae36c8913adb8e6d5e38f
Julio Obelleiro <julio.obelleiro@gmail.com>
2010-09-29 16:23:13 +0000

    - .user file added

---
+++ example_projects/Advanced_Shadows/src/Advanced_Shadows.cpp
@@ -0,0 +1,95 @@
+#include "Cing.h"
+CREATE_APPLICATION( "Cing" );
+
+/*   
+ * This example shows how to use shadows in Cing (Stencil Shadows in this case)
+ */
+
+PointLight			light;
+Box					box;
+Sphere				sphere1, sphere2, sphere3;
+float				angle = 0.02;
+Box					ground;
+bool				shadowsOn = true;
+
+void setup()
+{
+	size(1024, 768);
+	background(0);
+	enableShadows( STENCIL_MODULATIVE );
+
+	// Setup lighting
+	ambientLight(10);
+	light.init(255, 255, 255, width/2, height/2 + 100, 0);
+	
+	// Uncomment this line to see the position of the light
+	//light.drawDebug();
+
+	// Init 3d object's properties
+	box.init(50, 25, 75);
+	box.setPosition(width/2.0f, height/2.0f);
+	sphere1.init(30);
+	sphere2.init(60);
+	sphere3.init(100);
+
+	// Now, we make the spheres children of the box's (scene node)
+	box.addChild( sphere1 );
+	box.addChild( sphere2 );
+	box.addChild( sphere3 );
+
+	// Translate the sphere (relative to its parent, the box)
+	// This way, when we rotate the box (parent object), the spheres will orbitate around it
+	sphere1.setPosition( 2, -2, 0 );
+	sphere2.setPosition( 5, -7, 0 );
+	sphere3.setPosition( 7, -12, 0 );
+	
+	// Ground
+	ground.init(5000, 25, 5000 );
+	ground.setPosition( width/2, -200 );
+
+	// Camera control
+	enableDefault3DCameraControl();
+}
+
+void draw()
+{
+	// Rotate box and position (and the spheres will orbitate around it)
+	box.rotateY( angle );
+}
+
+void end()
+{
+}
+
+void mousePressed()
+{
+}
+
+void mouseMoved()
+{
+}
+
+void mouseReleased()
+{
+}
+
+void keyPressed()
+{
+	// Turn light shadows on/off
+	if ( key == 'o' )
+	{
+		shadowsOn = !shadowsOn;
+		light.castShadows(shadowsOn);
+	}
+	// You can control the light attenuation over distance
+	else if ( key == '1' )
+	{
+		// With some attenuation
+		light.setAttenuation( 100000, 1, 0.0025, 0 );
+	}
+	else if ( key == '2' )
+	{
+		// With no attenuation
+		light.setAttenuation( 100000, 1, 0, 0 );
+	}
+}
