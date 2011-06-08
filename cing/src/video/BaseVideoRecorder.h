a2772366ec6b6899f2b7284ac793a8e83112c250
jcanofuentes <jcanofuentes@gmail.com>
2011-06-08 20:44:42 +0200

    Video writing functionality added: BaseVideoRecorder and OCVVideoRecorder classes

---
+++ cing/src/video/BaseVideoRecorder.h
@@ -0,0 +1,60 @@
+#ifndef _BaseVideoRecorder_H_
+#define _BaseVideoRecorder_H_
+
+// Precompiled headers
+#include "Cing-Precompiled.h"
+
+// Cing
+#include "graphics\GraphicsTypes.h"
+#include "graphics\Image.h"
+
+namespace Cing
+{
+
+	/**
+	* @internal
+	* Base class for all the video recorder implementations.
+	*/
+	class BaseVideoRecorder
+	{
+	public:
+
+		// Constructor / Destructor
+		BaseVideoRecorder ();
+		virtual ~BaseVideoRecorder();
+
+		// Init / Release / Update (to be implemented in subclasses)
+		virtual bool		init            ( int width, int height, std::string fileName);
+		virtual bool		init            ( int width, int height, std::string fileName, int fps);
+
+		//virtual bool		init            ( std::string outputFileName = "", int width = 320, int height = 240, int fps = 25, Cing::GraphicsType format = Cing::RGB );
+
+		virtual void		end             ();
+		virtual void		update          () = 0;
+
+		// Video writing control
+		virtual void		addFrame        ( Cing::Image& newFrame ) = 0;
+		virtual void		finish			() = 0;
+
+		// Query methods
+		bool				isValid			() const { return m_bIsValid;   }
+		int					getWidth        () const { return m_width;      }
+		int					getHeight       () const { return m_height;     }
+		int					getFPS          () const { return m_fps;        }
+
+	protected:
+
+		// Attributes
+		std::string			m_fileName;					///< Output file name	
+		int					m_fps;                		///< Video frames per second
+		int					m_width, m_height;    		///< Video resolution
+
+	private:
+
+		// Attributes
+		bool				m_bIsValid;	          		///< Indicates whether the class is valid or not. If invalid none of its methods except init should be called.
+	};
+
+} // namespace
+
+#endif // _BaseVideoRecorder_H_
---
+++ cing/src/video/OCVVideoRecorder.cpp
@@ -0,0 +1,160 @@
+// Precompiled headers
+#include "Cing-Precompiled.h"
+
+#include "OCVVideoRecorder.h"
+
+// OpenCv
+#include "OpenCV/cv.h"
+
+// Common
+#include "common/Exception.h"
+#include "common/Release.h"
+#include "common/LogManager.h"
+
+namespace Cing
+{
+
+	/**
+	* @internal
+	* @brief Constructor. Initializes class attributes.
+	*/
+	OCVVideoRecorder::OCVVideoRecorder():
+m_bIsValid        ( false )
+{
+}
+
+/**
+* @internal
+* @brief Destructor. Class release.
+*/
+OCVVideoRecorder::~OCVVideoRecorder()
+{
+	// Release resources
+	end();
+}
+
+/**
+* @internal
+* @brief Initializes the class so it becomes valid.
+*
+* @param[in] width		width resolution capture
+* @param[in] height		height resolution capture
+* @param[in] fileName	Output file name
+*/
+bool OCVVideoRecorder::init( int width, int height ,std::string fileName)
+{
+	// Check if the class is already initialized
+	if ( isValid() )
+		return true;
+
+	// Init base class (with actual capture resolution)
+	BaseVideoRecorder::init(  width, height,fileName );
+
+	// Init OpenCv video recorder. Open a dialog box for CODEC selection...
+	m_cvVideoWriter = new cv::VideoWriter( m_fileName.c_str(), -1, m_fps, cvSize( m_width, m_height), true );
+
+	// TODO: Check CV_FOURCC manual initialization failure
+	//m_cvVideoWriter = new cv::VideoWriter( m_fileName.c_str(), CV_FOURCC('C', 'V', 'I', 'D'), m_fps, cvSize( m_width, m_height), isColor );
+
+	if ( m_cvVideoWriter->isOpened() )
+		m_bIsValid = true;	// The class is now initialized
+	else
+		m_bIsValid = false;
+
+	return m_bIsValid;
+}
+
+/**
+* @internal
+* @brief Initializes the class so it becomes valid.
+*
+* @param[in] width		Width resolution
+* @param[in] height		Height resolution
+* @param[in] fileName	Output file name
+* @param[in] fps		FPS
+* @return true if the initialization was ok | false otherwise
+*/
+bool OCVVideoRecorder::init( int width, int height ,std::string fileName, int fps/* = 25 */)
+{
+	// Check if the class is already initialized
+	if ( isValid() )
+		return true;
+
+	// Init base class (with actual capture resolution)
+	BaseVideoRecorder::init( width, height, fileName, fps );
+
+	// Init OpenCv video recorder. Open a dialog box for CODEC selection...
+	m_cvVideoWriter = new cv::VideoWriter( m_fileName.c_str(), -1, m_fps, cvSize( m_width, m_height), true );
+
+	// TODO: Check CV_FOURCC manual initialization failure
+	//m_cvVideoWriter = new cv::VideoWriter( m_fileName.c_str(), CV_FOURCC('C', 'V', 'I', 'D'), m_fps, cvSize( m_width, m_height), isColor );
+
+	if ( m_cvVideoWriter->isOpened() )
+		m_bIsValid = true;	// The class is now initialized
+	else
+		m_bIsValid = false;
+
+	return m_bIsValid;
+}
+
+/**
+* @internal
+* @brief Releases the class resources. After this method is called the class is not valid anymore.
+*/
+void OCVVideoRecorder::end()
+{
+	// Check if the class is already released
+	if ( !isValid() )
+		return;
+
+	// The class is not valid anymore
+	m_bIsValid = false;
+}
+
+/**
+* @internal
+*/
+void OCVVideoRecorder::update()
+{
+}
+
+/**
+* @public
+* @brief Add a new frame
+*/
+void OCVVideoRecorder::addFrame( Cing::Image& newFrame )
+{
+	bool isOpen = m_cvVideoWriter->isOpened();
+
+	if (isOpen)
+	{
+		// HACK: Convert RGB2BGR then BGR2RGB
+		cvtColor(newFrame.getCVMat(), newFrame.getCVMat(), CV_RGB2BGR);
+		(*m_cvVideoWriter) << newFrame.getCVMat();
+		cvtColor(newFrame.getCVMat(), newFrame.getCVMat(), CV_BGR2RGB);
+	}
+	else
+		LOG_CRITICAL("OCVVideoRecorder::addFrame() - Warning: OCVVideoRecorder is closed");
+}
+
+/**
+* @public
+* @brief Close the file and writes to disk
+*/
+void OCVVideoRecorder::finish()
+{
+	// File is written to disk when calling the destructor
+	delete m_cvVideoWriter;
+	m_cvVideoWriter = NULL;
+
+	end();
+
+	/*
+	// TODO: Check this
+	// Create a new cv::VideoWriter to allow future use of this object
+	m_cvVideoWriter = new cv::VideoWriter( m_fileName.c_str(), -1, m_fps, cvSize( m_width, m_height), true );
+	*/
+}
+
+
+} // namespace
