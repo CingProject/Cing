8911f287f5320b9720bc3519add1244526725b8d
Shawn Roske <sroske@bitgun.com>
2013-08-05 16:54:09 -0500

    added Simple_userApplicationCocoaView example stub

---
+++ example_projects/Simple_UserApplicationCocoaView/xcode/Simple_UserApplicationCocoaView/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Simple_UserApplicationCocoaView
+//
+//  Created by shawn.roske on 8/5/13.
+//  Copyright (c) 2013 Cing Project. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
