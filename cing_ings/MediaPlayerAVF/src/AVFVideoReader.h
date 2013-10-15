50f03c32e9bfa87d4f96ead596a2156a5cd2ddb5
julioobelleiro <julio.obelleiro@wildbytes.cc>
2013-10-15 17:46:54 -0400

    Added AVFVideoReader to process video frames from a movie file

---
+++ cing_ings/MediaPlayerAVF/src/AVFVideoReader.h
@@ -0,0 +1,13 @@
+//
+//  AVFVideoReader.h
+//  Cing
+//
+//  Created by Julio Obelleiro on 10/14/13.
+//  Copyright (c) 2013 Cing Project. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AVFVideoReader : NSObject
+
+@end
---
+++ cing_ings/MediaPlayerAVF/src/AVFVideoReader.mm
@@ -0,0 +1,13 @@
+//
+//  AVFVideoReader.m
+//  Cing
+//
+//  Created by Julio Obelleiro on 10/14/13.
+//  Copyright (c) 2013 Cing Project. All rights reserved.
+//
+
+#import "AVFVideoReader.h"
+
+@implementation AVFVideoReader
+
+@end
